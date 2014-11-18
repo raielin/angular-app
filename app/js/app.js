2a1520b90bf5233561ab1218a08dc22097ff6ad2
raielin@gmail.com <raielin@gmail.com>
2014-11-18 16:12:19 -0500

    Pulled out user factory into its own file and added script tag to HTML

--- app/js/app.js
+++ app/js/app.js
@@ -49,26 +49,6 @@ angular.module('StaffingUI').config(function($routeProvider) {
         });
 });
 
-// Factory allows us to list out our titles in the dropdown
-// Unless you have a good reason to use a service, it's better to use a Factory
-// We use a factory for sharing data or functionality between controllers.
-// Allows us to keep our logic out of our controllers.
-
-angular.module('StaffingUI').factory('UserFactory', function($http) {
-  var users = [];
-
-  var fetch = function() {
-    $http.get('http://localhost:3000/users').success(function(response) {
-      angular.copy(response, users);
-    });
-  };
-
-  return {
-    users: users,
-    fetch: fetch
-  };
-});
-
 angular.module('StaffingUI').factory('TitleFactory', function($http) {
   var titles = [];
 
--- app/js/navbar.controller.js
+++ app/js/navbar.controller.js
@@ -1,5 +1,11 @@
 // .controller in the filename is a convention that's used by Yeoman
 // good practice, but not necessary.
+
+// Can be organized by function, i.e. login, nav, etc.
+// , or by type, i.e. controller, factory, etc.
+
+// Angular will look for the module. So as long as the module is defined,
+// Angular will know to associate it.
 angular.module('StaffingUI').controller('NavbarCtrl', function($scope, $location) {
     'use strict';
 
